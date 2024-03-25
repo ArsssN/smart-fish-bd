1dd17132f2227ebf13ed35a6a4191e6fc1f06064
AfzalSabbir <afzalbd1@gmail.com>
2024-03-25 15:13:42 +0600

    New seed
    - Switch
    - Sensor
    - Pond

--- database/seeders/seeder-data/pond_switch_unit.php
+++ database/seeders/seeder-data/pond_switch_unit.php
@@ -2,23 +2,83 @@
 $pond_switch_unit = array (
   0 => 
   array (
-    'pond_id' => 2,
-    'switch_unit_id' => 1,
+    'pond_id' => 4,
+    'switch_unit_id' => 5,
   ),
   1 => 
   array (
-    'pond_id' => 1,
-    'switch_unit_id' => 2,
+    'pond_id' => 6,
+    'switch_unit_id' => 12,
   ),
   2 => 
   array (
-    'pond_id' => 1,
-    'switch_unit_id' => 1,
+    'pond_id' => 9,
+    'switch_unit_id' => 11,
   ),
   3 => 
   array (
+    'pond_id' => 1,
+    'switch_unit_id' => 6,
+  ),
+  4 => 
+  array (
     'pond_id' => 3,
+    'switch_unit_id' => 8,
+  ),
+  5 => 
+  array (
+    'pond_id' => 7,
     'switch_unit_id' => 2,
+  ),
+  6 => 
+  array (
+    'pond_id' => 4,
+    'switch_unit_id' => 4,
+  ),
+  7 => 
+  array (
+    'pond_id' => 4,
+    'switch_unit_id' => 3,
+  ),
+  8 => 
+  array (
+    'pond_id' => 5,
+    'switch_unit_id' => 4,
+  ),
+  9 => 
+  array (
+    'pond_id' => 8,
+    'switch_unit_id' => 8,
+  ),
+  10 => 
+  array (
+    'pond_id' => 9,
+    'switch_unit_id' => 9,
+  ),
+  11 => 
+  array (
+    'pond_id' => 1,
+    'switch_unit_id' => 7,
+  ),
+  12 => 
+  array (
+    'pond_id' => 4,
+    'switch_unit_id' => 10,
+  ),
+  13 => 
+  array (
+    'pond_id' => 4,
+    'switch_unit_id' => 1,
+  ),
+  14 => 
+  array (
+    'pond_id' => 2,
+    'switch_unit_id' => 9,
+  ),
+  15 => 
+  array (
+    'pond_id' => 10,
+    'switch_unit_id' => 3,
   ),
 );
 return $pond_switch_unit;
