b25b84cf2441a41151279773f9ffe044ef5bc991
AfzalSabbir <afzalbd1@gmail.com>
2024-03-25 17:43:00 +0600

    Update l5-swagger.php

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => config('app.name') . ' - API Documentation',
             ],
 
             'routes' => [
