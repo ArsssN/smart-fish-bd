5a3d26a71b2d9444ee5950be603bca90ce1865d8
AfzalSabbir <Afzalur.Rahman@brainstation-23.com>
2024-04-04 17:49:55 +0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,46 +14,41 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $this->call([
-            UserSeeder::class,
-            UserDetailsSeeder::class,
-            // SettingSeeder::class,
-            PermissionSeeder::class,
-            AboutSeeder::class,
-            FooterLinkGroupSeeder::class,
-            FooterLinkSeeder::class,
-            SocialSeeder::class,
-            SensorTypeSeeder::class,
-            SensorSeeder::class,
-            SensorUnitSeeder::class,
-            ControllerSeeder::class,
-            ProjectSeeder::class,
-            SensorControllerSeeder::class,
-            ControllerProjectSeeder::class,
-            ProjectSensorSeeder::class,
-            FishSeeder::class,
-            FishWeightSeeder::class,
-            FeederSeeder::class,
-            AeratorSeeder::class,
-            FeederProjectSeeder::class,
-            AeratorProjectSeeder::class,
-            FeederHistorySeeder::class,
-            PondSeeder::class,
-            PondSensorUnitSeeder::class,
-            SensorTypeSensorUnitSeeder::class,
-            SwitchTypeSeeder::class,
-            SwitchSeeder::class,
-            SwitchUnitSeeder::class,
-            PondSwitchUnitSeeder::class,
-            MqttDataSeeder::class,
-            MqttDataHistorySeeder::class,
-        ]);
+        if (app()->environment('local')) {
+            $this->call([
+                UserSeeder::class,
+                UserDetailsSeeder::class,
+                // SettingSeeder::class,
+                PermissionSeeder::class,
+                AboutSeeder::class,
+                FooterLinkGroupSeeder::class,
+                FooterLinkSeeder::class,
+                SocialSeeder::class,
+                SensorTypeSeeder::class,
+                SensorSeeder::class,
+                SensorUnitSeeder::class,
+                ControllerSeeder::class,
+                ProjectSeeder::class,
+                SensorControllerSeeder::class,
+                ControllerProjectSeeder::class,
+                ProjectSensorSeeder::class,
+                FishSeeder::class,
+                FishWeightSeeder::class,
+                FeederSeeder::class,
+                AeratorSeeder::class,
+                FeederProjectSeeder::class,
+                AeratorProjectSeeder::class,
+                FeederHistorySeeder::class,
+                PondSeeder::class,
+                PondSensorUnitSeeder::class,
+                SensorTypeSensorUnitSeeder::class,
+                SwitchTypeSeeder::class,
+                SwitchSeeder::class,
+                SwitchUnitSeeder::class,
+                PondSwitchUnitSeeder::class,
+                MqttDataSeeder::class,
+                MqttDataHistorySeeder::class,
+            ]);
+        }
     }
 }
