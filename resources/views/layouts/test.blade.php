fe731e4dbab1823d5962773255fd224d836e8c36
AfzalSabbir <afzalbd1@gmail.com>
2024-08-24 01:46:34 +0600

    separated test routes

--- resources/views/layouts/test.blade.php
+++ resources/views/layouts/test.blade.php
@@ -23,9 +23,8 @@
     </style>
 </head>
 <body class="antialiased">
-<div class="container">
-    @yield('content')
-</div>
+@include('test.partial.nav')
+@yield('content')
 {{-- Bootstrap 5 latest CDN --}}
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
