2fb5aadf0734fabd1b137b9650e7c77df6b2668b
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2024-08-29 12:35:36 +0600

    Temperature and Oxygen Sensor logic updated

--- app/Helpers/SensorHelpers/OxygenSensor.php
+++ app/Helpers/SensorHelpers/OxygenSensor.php
@@ -5,15 +5,15 @@ if (!function_exists('getOxygenSensorUpdate')) {
     {
         $sensorList = array();
 
-        if ($value < 3)
+        if ($value <= 3)
             $sensorList = [1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0];
-        else if ($value < 4.5)
+        else if ($value <= 4.5)
             $sensorList = [0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0];
-        else if ($value < 6)
+        else if ($value <= 6)
             $sensorList = [1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0];
-        else if ($value > 6 && $value < 8)
+        else if ($value > 6 || $value < 7)
             $sensorList = [0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0];
-        else if ($value >= 8)
+        else if ($value >= 7)
             $sensorList = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         /*else
             return "Invalid DO level. Please enter a value between 0 and 8.";*/
--- app/Helpers/SensorHelpers/TemperatureSensor.php
+++ app/Helpers/SensorHelpers/TemperatureSensor.php
@@ -5,7 +5,7 @@ if (!function_exists('getTemperatureSensorUpdate')) {
     {
         $fishFeeder = [];
 
-        if ($value < 24) {
+        /*if ($value < 24) {
             // $fishFeeder = '1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0';
             $fishFeeder = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         } else if ($value >= 24 && $value <= 28) {
@@ -13,6 +13,16 @@ if (!function_exists('getTemperatureSensorUpdate')) {
         } else if ($value > 28) {
             // $fishFeeder = '1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0';
             $fishFeeder = [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0];
+        }*/
+
+        if ($value < 20 || $value > 38) {
+            return 'No Feed';
+        }elseif ($value >= 20 || $value <= 25) {
+            return 'Below';
+        }elseif ($value > 25 || $value <= 32) {
+            return 'Optimum';
+        }elseif ($value > 32 || $value <= 38) {
+            return 'High';
         }
 
         return $fishFeeder;
