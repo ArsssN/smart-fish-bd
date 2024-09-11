ced2e37c34888d86a71832ca4aab821d9d866b7f
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2024-09-11 09:29:19 +0600

    Update AeratorManageCommand.php

--- app/Console/Commands/AeratorManageCommand.php
+++ app/Console/Commands/AeratorManageCommand.php
@@ -118,12 +118,12 @@ class AeratorManageCommand extends Command
     /**
      * @var int - start after time in min: 20
      */
-    const switchOnAfter = 20 * 60; // seconds
+    const switchOnAfter = 1 * 60; // seconds
 
     /**
      * @var int - stop after time in min: 40
      */
-    const switchOffAfter = 40 * 60; // seconds
+    const switchOffAfter = 2 * 60; // seconds
 
     /**
      * @var int - switch type id (aerator)
