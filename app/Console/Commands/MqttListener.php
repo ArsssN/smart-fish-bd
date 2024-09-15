aee18f656d1a56e8e806cf99133817637210b94e
AfzalSabbir <afzalbd1@gmail.com>
2024-09-16 00:49:23 +0600

    Update MqttListener.php

--- app/Console/Commands/MqttListener.php
+++ app/Console/Commands/MqttListener.php
@@ -43,32 +43,11 @@ class MqttListener extends Command
     protected string $message;
 
     /**
-     * The original message.
-     *
-     * @var string
-     */
-    protected static string $original_message;
-
-    /**
      * The topic to be subscribed to.
      *
      * @var string
      */
     protected string $topic;
-
-    /**
-     * The flag to check if the message is an update.
-     *
-     * @var bool
-     */
-    protected bool $isUpdate = false;
-
-    /**
-     * The flag to check if the command is in test mode.
-     *
-     * @var bool $isTest - flag to check if the command is in test mode
-     */
-    protected bool $isTest = false;
 
     /**
      * The current time.
@@ -159,158 +138,8 @@ class MqttListener extends Command
         return Command::SUCCESS;
     }
 
-    /**
-     * Process the response from the MQTT server.
-     *
-     * @return bool
-     */
-    public function processResponse(): bool
-    {
-        $this->currentDateTime = $this->currentDateTime ?? now()->format('Y-m-d H:i:s');
-        $this->currentTime = $this->currentTime ?? now()->format('H:i');
-        $this->isUpdate = false;
-        $responseMessage = json_decode($this->message);
-
-        self::$original_message = $this->message;
-
-        if (isset($responseMessage->data->o2) && $responseMessage->data->o2 < 1.5) {
-            $o2 = convertDOValue($responseMessage->data->o2, $this->currentTime);
-            $echo = 'Converted DO value: from ' . $responseMessage->data->o2 . ' to ' . $o2 . ' at ' . $this->currentTime . '<br>';;
-            echo $echo;
-            Log::channel('mqtt_listener')->info($echo);
-            $responseMessage->data->o2 = $o2;
-        }
-
-        if ($this->isTest) {
-            MqttCommandController::$isSaveMqttData = false;
-        }
-
-        if (isset($responseMessage->update)) {
-            sleep(2);
-            $this->isUpdate = true;
-            $gateway_serial_number_last_4digit = Str::before(Str::after($this->topic, '/'), '/');
-            $project = \App\Models\Project::query()
-                ->where('gateway_serial_number', 'LIKE', "%{$gateway_serial_number_last_4digit}")
-                ->firstOrFail();
-            $mqtt_data = $project->mqttData()
-                //->whereNotNull('publish_message')
-                ->orderBy('id', 'desc')
-                ->firstOrFail();
-            $responseMessage = json_decode($mqtt_data->data);
-            $publishMessage = json_decode($mqtt_data->publish_message);
-            MqttCommandController::$feedBackArray['relay'] = $publishMessage->relay
-                ?? implode('', array_fill(0, 12, 0));
-        }
-
-        if (!$this->isUpdate) {
-            switch ($responseMessage->type) {
-                case 'sen':
-                    MqttCommandController::saveMqttData('sensor', $responseMessage, $this->topic);
-                    break;
-                case 'swi':
-                    MqttCommandController::saveMqttData('switch', $responseMessage, $this->topic);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        $feedBackArr = MqttCommandController::$feedBackArray;
-        Log::channel('mqtt_listener')->info("Send message on topic [$this->topic]: " . $feedBackArr['relay']);
-        if (!$this->isTest) {
-            echo sprintf(
-                '[%s] Send message on topic [%s]: <strong>%s</strong>',
-                $this->currentDateTime,
-                $this->topic,
-                $feedBackArr['relay']
-            );
-            echo '<br/>';
-        }
-
-        $publishable = false;
-
-        if ($feedBackArr['relay'] !== implode(', ', array_fill(0, 12, 0))) {
-            if (!$this->isUpdate) {
-                $feedBackArr['relay'] = implode('', explode(', ', $feedBackArr['relay']));
-
-                if (MqttCommandController::$isSaveMqttData) {
-                    MqttCommandController::$mqttData->publish_message = json_encode($feedBackArr);
-                    MqttCommandController::$mqttData->save();
-                }
-            }
-            if (
-                $this->isUpdate
-                || (
-                    $responseMessage->type == 'sen'
-                    && !empty($responseMessage->data)
-                    && empty($responseMessage->relay)
-                )
-            ) {
-                $publishable = true;
-            }
-
-        }
-
-        MqttCommandController::$isAlreadyPublished = false;
-        if ($publishable && !$this->isUpdate) {
-            $previousMqttData = MqttData::query()
-                ->where('project_id', MqttCommandController::$mqttData->project_id)
-                ->where('id', '!=', MqttCommandController::$mqttData->id)
-                ->latest()
-                ->first();
-
-            $publishable = (MqttCommandController::$feedBackArray['relay'] ?? '')
-                !== (json_decode($previousMqttData->publish_message, true)['relay'] ?? '');
-
-            if (!$publishable) {
-                MqttCommandController::$isAlreadyPublished = true;
-
-                Log::channel('mqtt_listener')->info("Already published message on topic [$this->topic]: " . $feedBackArr['relay']);
-                if (!$this->isTest) {
-                    echo sprintf(
-                        '[%s] Already published message on topic [%s], mqtt data id: <strong>%d</strong>',
-                        $this->currentDateTime,
-                        $this->topic,
-                        $previousMqttData->id
-                    );
-                }
-            }
-        }
-
-        if (MqttCommandController::$switchUnitStatus !== 'active') {
-            $publishable = false;
-        }
-
-        return $publishable;
-    }
-
-    public function setIsUpdate(bool $isUpdate): void
-    {
-        $this->isUpdate = $isUpdate;
-    }
-
     public function setTopic(string $topic): void
     {
         $this->topic = $topic;
-    }
-
-    public function setMessage(string $message): void
-    {
-        $this->message = $message;
-    }
-
-    public function setCurrentTime(string $currentTime): void
-    {
-        $this->currentTime = $currentTime;
-    }
-
-    public function setIsTest(bool $isTest): void
-    {
-        $this->isTest = $isTest;
-    }
-
-    public static function getOriginalMessage(): string
-    {
-        return self::$original_message ?? '';
     }
 }
