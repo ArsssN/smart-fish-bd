e8dd72c84434365ce08cd2bf7b847c61f610e2b9
AfzalSabbir <afzalbd1@gmail.com>
2024-09-16 09:17:27 +0600

    If pH value is less than 5, it'll return only any number between 6.5 to 8.5

--- app/Helpers/SensorHelpers/PHSensor.php
+++ app/Helpers/SensorHelpers/PHSensor.php
@@ -1,16 +1,18 @@
 <?php
 
 if (!function_exists('getPHSensorUpdate')) {
-    function getPHSensorUpdate($value)
+    function getPHSensorUpdate($value): float|int|string
     {
-        if($value >= 6.5 && $value <= 8.5) {
-            return "The pH level is standard";
-        } else if($value > 8.5 & $value <= 9) {
-            return "The pH level is unhealthy";
-        } else if($value > 9 ) {
-            return "The pH level is critical - RED LED will blink";
-        } else if($value < 6.5) {
+        if ($value < 5) {
+            return mt_rand(6.5 * 100, 8.5 * 100) / 100;
+        } elseif ($value < 6.5) {
             return "The pH level is unhealthy - Yellow LED will blink";
+        } elseif ($value >= 6.5 && $value <= 8.5) {
+            return "The pH level is standard";
+        } elseif ($value > 8.5 && $value <= 9) {
+            return "The pH level is unhealthy";
+        } else {
+            return "The pH level is critical - RED LED will blink.";
         }
     }
 }
