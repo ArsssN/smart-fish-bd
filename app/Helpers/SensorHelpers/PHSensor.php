ae6c9815e73c7ea7d2d70a5cb7856c9d60126b91
Ekramul Islam Sumon <38817030+sumoninfo@users.noreply.github.com>
2024-09-18 10:19:24 +0600

    Update PHSensor.php

--- app/Helpers/SensorHelpers/PHSensor.php
+++ app/Helpers/SensorHelpers/PHSensor.php
@@ -3,14 +3,14 @@
 if (!function_exists('getPHSensorUpdate')) {
     function getPHSensorUpdate($value): float|int|string
     {
-        if ($value < 6.5) {
-            return "The pH level is unhealthy - Yellow LED will blink";
-        } elseif ($value >= 6.5 && $value <= 8.5) {
-            return "The pH level is standard";
-        } elseif ($value > 8.5 && $value <= 9) {
-            return "The pH level is unhealthy";
+        if ($value < 7) {
+            return 'The pH level is unhealthy - Yellow LED will blink';
+        } elseif ($value <= 8) {
+            return 'The pH level is standard';
+        } elseif ($value <= 9) {
+            return 'The pH level is unhealthy';
         } else {
-            return "The pH level is critical - RED LED will blink.";
+            return 'The pH level is critical - RED LED will blink.';
         }
     }
 }
